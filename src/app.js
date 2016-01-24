9d7d7506c5804bea9fca5eee9ffefa1d90c989a0
nicolasmir <elmirnicolas@gmail.com>
2016-01-25 01:08:34 +0200

    pebble

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 var UI = require('ui');
 var ajax = require('ajax');
-//var Accel = require('ui/accel');
-//var Vibe = require('ui/vibe');
+var Accel = require('ui/accel');
 
 
 var card = new UI.Card({
-  title:'time'
- // subtitle:''
+  title:'Getting tweets ...',
+ subtitle:''
 });
 
 
@@ -51,4 +50,51 @@ ajax(
   }
 );
 
+card.on('accelTap', function(e) {
+  var Time = new Date();
+ var hours = Time.getHours();
+ var minutes = Time.getMinutes();
+ var URL = 'http://alltheminutes.com/tweets/' + hours +'/'+ minutes +'.json';
+  
+  ajax(
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(id_data) {
+    var len = id_data.length;
+    var i =  Math.floor(Math.random() * len);
+    
+    var dataId = id_data[i];
+    
+ 
+ var URL2 = 'http://nodejs-evilqubit.rhcloud.com/' + dataId;   
+     ajax(
+  {
+    url: URL2,
+    type: 'json'
+  },
+  function(tweetsData) {
+   // var len = tweetsData.length;
+   // var i =  Math.floor(Math.random() * len);
+    
+   var dataTweets = tweetsData;
+    //var d = new Date();
+    card.body(dataTweets.text);
+    card.title(dataTweets.user.name);
+    card.subtitle('@' + dataTweets.user.screen_name);
+  }
+);
+  }
+);
+  console.log("Okay its working");
+});
+
+
+//static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  // Update things here
+
+//}
+
+Accel.init();
 
